54a3756ef256fb1c9b055be1c3e0077fea62173a
Alex Náger <51228484+aleexnager@users.noreply.github.com>
2024-06-30 20:46:31 +0200

    Add files via upload

---
+++ WebApplication5/WebApplication5/Controllers/Friends.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class Friends : Controller
+    {   
+        // GET: /Friends/
+        public IActionResult Index() => Content("FriendsController.Index()");
+
+        // GET: /Friends/View/John
+        public IActionResult View(string name) => Content($"FriendsController.View('{name}')");
+
+        // GET: /Friends/Edit/23
+        public IActionResult Edit(int id) => Content($"FriendsController.Edit('{id}')");
+
+        // GET: /delete/friends/18
+        public IActionResult Delete(int id) => Content($"FriendsController.Delete('{id}')");
+    }
+}
